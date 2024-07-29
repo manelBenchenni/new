a31ba2dda3f9205ee2066bc75a4c7752d0ea4ad4
manelBenchenni <144050881+manelBenchenni@users.noreply.github.com>
2024-07-29 14:17:16 +0100

    Update urls.py

--- myFirstProject/urls.py
+++ myFirstProject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('mymainApp.urls'))
+    path('https://manelbenchenni.github.io/new/',include('mymainApp.urls'))
 ]
